c6b356b1cf99a477c02544f01a78eaed1cb20ecd
A24Jav <a24javsalcue@inspedralbes.cat>
2026-04-03 15:29:06 +0200

    Traducción añadida a los nombres de los objetos

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.Localization;
 
 [CreateAssetMenu(menuName = ("Item/Create Item"))]
 public class Item : ScriptableObject
 {
     public Sprite itemSpr;
-    public string itemName;
+    public LocalizedString ItemLocalName;
+    public string itemName => ItemLocalName.GetLocalizedString();
     public int itemPrice;
     public ItemFunction itemFunction;
     public Inventory inventoryAssociated;
