15f624d6431c6ddb1bcd9241c521ce40e1c13de9
RaulRedondo-06 <a24rauredrio@inspedralbes.cat>
2026-04-06 18:24:07 +0200

    sa
    sa

---
+++ Assets/Scripts/Menu/CreditController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditosController: MonoBehaviour
+{
+    [Header("Nombres de escenas")]
+    [SerializeField] private string atras;
+
+    public void IrAtras()
+    {
+        SceneManager.LoadScene(atras);
+    }
+}
