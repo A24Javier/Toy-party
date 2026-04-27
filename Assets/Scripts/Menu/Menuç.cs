6f7c36335ddf70543fd39fcf72b60f198ced1e69
RaulRedondo-06 <a24rauredrio@inspedralbes.cat>
2026-04-27 18:25:04 +0200

    nuevo menu
    nuevo menu

--- Assets/Scripts/Menu/Menuç.cs
+++ Assets/Scripts/Menu/Menuç.cs
@@ -5,30 +5,12 @@ using UnityEngine.Events;
 
 public class Logo : MonoBehaviour
 {
-    [System.Serializable]
-    public class BotonAnimado
-    {
-        public RectTransform boton;
-        public float posicionFinalY;
-    }
-
-    [Header("Movimiento Logo")]
-    [SerializeField] private float posicionFinalY = 200f;
-    [SerializeField] private float duracionMov = 1.5f;
-    [SerializeField] private float reboteH = 30f;
-    [SerializeField] private float duracionReb = 0.15f;
-    [SerializeField] private float duracionSegundoMov = 0.2f;
-    [SerializeField] private RectTransform logo;
-
     [Header("Movimiento Cortinas")]
     [SerializeField] private float posicionFinalCor = 1000f;
     [SerializeField] private float duracionMovCor = 2.5f;
     [SerializeField] private RectTransform cor1;
     [SerializeField] private RectTransform cor2;
     [SerializeField] private RectTransform corFondo;
-
-    [Header("Botones")]
-    [SerializeField] private List<BotonAnimado> botones = new List<BotonAnimado>();
 
     [Header("Evento")]
     public UnityEvent OnMoveFinished;
@@ -49,44 +31,6 @@ public class Logo : MonoBehaviour
 
         if (corFondo != null)
             seq.Join(corFondo.DOAnchorPosY(posicionFinalCor, duracionMovCor).SetEase(Ease.InQuad));
-
-        seq.OnComplete(() =>
-        {
-            Move();
-        });
-    }
-
-    public void Move()
-    {
-        if (logo != null)
-            logo.DOKill();
-
-        for (int i = 0; i < botones.Count; i++)
-        {
-            if (botones[i].boton != null)
-                botones[i].boton.DOKill();
-        }
-
-        Sequence seq = DOTween.Sequence();
-
-        if (logo != null)
-        {
-            seq.Append(logo.DOAnchorPosY(posicionFinalY, duracionMov).SetEase(Ease.InQuad));
-            seq.Append(logo.DOAnchorPosY(posicionFinalY + reboteH, duracionReb).SetEase(Ease.OutQuad));
-            seq.Append(logo.DOAnchorPosY(posicionFinalY, duracionSegundoMov).SetEase(Ease.InQuad));
-        }
-
-        for (int i = 0; i < botones.Count; i++)
-        {
-            BotonAnimado botonActual = botones[i];
-
-            if (botonActual.boton == null)
-                continue;
-
-            seq.Join(
-                botonActual.boton.DOAnchorPosY(botonActual.posicionFinalY, duracionMov).SetEase(Ease.InQuad)
-            );
-        }
 
         seq.OnComplete(() =>
         {
