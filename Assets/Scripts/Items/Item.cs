2860953c13aec02de77103393a2ee80a458147a1
A24Javier <a24javsalcue@inspedralbes.cat>
2026-05-29 11:12:13 +0200

    Haciendo feedback de los objetos

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -8,9 +8,12 @@ public class Item : ScriptableObject
     public Sprite itemSpr;
 
     public LocalizedString ItemLocalName;
+    public LocalizedString ItemLocalDescription;
     
     [SerializeField] private string _itemName;
+    [SerializeField] private string _itemDescription;
     public string ItemName => _itemName;
+    public string ItemDescription => _itemDescription;
 
     public int itemPrice;
     public ItemFunction itemFunction;
@@ -26,6 +29,7 @@ public class Item : ScriptableObject
     private void OnDisable()
     {
         ItemLocalName.StringChanged -= UpdateName;
+        ItemLocalDescription.StringChanged -= UpdateDescription;
 
         LocalizationSettings.SelectedLocaleChanged -= OnLocaleChanged;
 
@@ -40,17 +44,25 @@ public class Item : ScriptableObject
         _initialized = true;
 
         ItemLocalName.StringChanged += UpdateName;
+        ItemLocalDescription.StringChanged += UpdateDescription;
         LocalizationSettings.SelectedLocaleChanged += OnLocaleChanged;
         ItemLocalName.RefreshString();
+        ItemLocalDescription.RefreshString();
     }
 
     private void OnLocaleChanged(Locale locale)
     {
         ItemLocalName.RefreshString();
+        ItemLocalDescription.RefreshString();
     }
 
     private void UpdateName(string value)
     {
         _itemName = value;
+    }
+
+    private void UpdateDescription(string value)
+    {
+        _itemDescription = value;
     }
 }
