696a6a6b62393a27d49d0207577b3c4e088977fb
JonnyCuartero32 <a23jonauscua@inspedralbes.cat>
2026-06-05 20:28:25 +0200

    update

--- Assets/Minijuegos/Silla a Sevilla/Scripts/ManagerTimeLine.cs
+++ Assets/Minijuegos/Silla a Sevilla/Scripts/ManagerTimeLine.cs
@@ -1,3 +1,4 @@
+using FMODUnity;
 using UnityEngine;
 using UnityEngine.Playables;
 
@@ -5,14 +6,16 @@ public class ManagerTimeLine : MonoBehaviour
 {
     PlayableDirector director;
 
-    [SerializeField] AudioSource MiMusica;
-    [SerializeField] AudioSource MusicaAddaptible;
+    [Header("Asigna tus GameObjects de Audio aquí")]
+    [SerializeField] private StudioEventEmitter emisorMiMusica;
+    [SerializeField] private StudioEventEmitter emisorMusicaAdaptible;
 
     void Awake()
     {
         director = GetComponent<PlayableDirector>();
     }
 
+    #region Control de Timeline
     public void PausarAnimation()
     {
         if (director != null)
@@ -27,16 +30,42 @@ public class ManagerTimeLine : MonoBehaviour
             director.Play();
         }
     }
+    #endregion
 
+    #region Control de Música Especial
     public void ActivarMusicSpecial()
     {
-        if (MusicaAddaptible != null && !MusicaAddaptible.isPlaying)
-            MusicaAddaptible.Play();
+        // Comprobamos si el emisor está asignado y si NO está sonando ya
+        if (emisorMusicaAdaptible != null && !emisorMusicaAdaptible.IsPlaying())
+        {
+            emisorMusicaAdaptible.Play();
+        }
     }
 
     public void DesactivarMusicSpecial()
     {
-        if (MusicaAddaptible != null)
-            MusicaAddaptible.Stop();
+        if (emisorMusicaAdaptible != null && emisorMusicaAdaptible.IsPlaying())
+        {
+            emisorMusicaAdaptible.Stop();
+        }
     }
+    #endregion
+
+    #region Control de Música Base
+    public void ActivarMusicaBase()
+    {
+        if (emisorMiMusica != null && !emisorMiMusica.IsPlaying())
+        {
+            emisorMiMusica.Play();
+        }
+    }
+
+    public void DesactivarMusicaBase()
+    {
+        if (emisorMiMusica != null && emisorMiMusica.IsPlaying())
+        {
+            emisorMiMusica.Stop();
+        }
+    }
+    #endregion
 }
